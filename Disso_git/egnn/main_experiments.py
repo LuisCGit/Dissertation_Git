ff6f68e7467ac3eb0eb039746a5fe95a20241bbe
LuisCGit <luis.costa.laveron@gmail.com>
2021-02-16 01:10:08 +0100

    Citeseer learncurvs

--- Disso_git/egnn/main_experiments.py
+++ Disso_git/egnn/main_experiments.py
@@ -46,8 +46,8 @@ parser.add_argument('--adaptive', action='store_true', default=False)
 parser.add_argument('--weighted', action='store_true', default=False)
 
 #Luis added args
-parser.add_argument('--num-trials', type=int, default=1)
-alpha_vals = np.linspace(0,0.5,11)
+parser.add_argument('trials', type=int, default=5)
+alpha_vals = np.linspace(0,1,11)
 curvature_mapping_alpha = [1.0,4.0]
 normalization = ['dsm','row']
 
@@ -89,162 +89,170 @@ edge_feat_list = [ollivier_curv_vals,forman_curv_vals]
 ########################################### END ###########################################
 
 #datasets = ['citeseer'] #,'pubmed']
-results = np.zeros((len(alpha_vals),len(curvature_mapping_alpha),len(normalization), args.epochs, 3)) #val loss, val acc, tes acc
-for idx, param_tup in enumerated_product(alpha_vals, curvature_mapping_alpha, normalization):
-    print("param tup: ", param_tup)
-    print("idx: ", idx)
-    alpha_val,curv_mapping_alpha,norm = alpha_vals[5], 1, 'row'#param_tup
-    args.edge_norm = norm
+#(for hyperparam search) results = np.zeros((len(alpha_vals),len(curvature_mapping_alpha),len(normalization), args.epochs, 3)) #val loss, val acc, tes acc
+results = np.zeros((len(alpha_vals),args.trials,args.epochs,4))
+#(for hyperparam search)for idx, param_tup in enumerated_product(alpha_vals, curvature_mapping_alpha, normalization):
+for alpha_idx, alpha_val in enumerate(alpha_vals):
+    for trial in range(args.trials):
+        print("param tup: ", param_tup)
+        print("idx: ", idx)
+        #below vals for pubmed
+        #curv_mapping_alpha,norm = alpha_vals[5], 1, 'row'#param_tup
+        #below vals for citeseer
+        curv_mapping_alpha,norm = 1, 'dsm'
+        args.edge_norm = norm
 
-    # ************************************************************
-    # calculate node features
-    # ************************************************************
-    vals = []
-    X = X.astype(np.float32)
-    EYE = scipy.sparse.eye(K, dtype=np.float32, format='coo')
-    A = A.astype(np.float32)
+        # ************************************************************
+        # calculate node features
+        # ************************************************************
+        vals = []
+        X = X.astype(np.float32)
+        EYE = scipy.sparse.eye(K, dtype=np.float32, format='coo')
+        A = A.astype(np.float32)
 
-    # normalized x
-    rowsum = sparse.as_coo(X).sum(axis=-1, keepdims=True)
-    #rowsum.data[rowsum.data==0] = 1e-10
-    rowsum.data = 1.0 / rowsum.data
-    vals.append((sparse.as_coo(X) * rowsum).to_scipy_sparse())
-    nodes = scipy.sparse.hstack(vals)
-    nodes = nodes.toarray()
+        # normalized x
+        rowsum = sparse.as_coo(X).sum(axis=-1, keepdims=True)
+        #rowsum.data[rowsum.data==0] = 1e-10
+        rowsum.data = 1.0 / rowsum.data
+        vals.append((sparse.as_coo(X) * rowsum).to_scipy_sparse())
+        nodes = scipy.sparse.hstack(vals)
+        nodes = nodes.toarray()
 
-    # ************************************************************
-    # calculate edge features
-    # ************************************************************
-    vals = []
-    EYE = scipy.sparse.eye(K, dtype=np.float32, format='coo')
+        # ************************************************************
+        # calculate edge features
+        # ************************************************************
+        vals = []
+        EYE = scipy.sparse.eye(K, dtype=np.float32, format='coo')
 
-    for mat in edge_feat_list:
-        vals.append((mat+mat.transpose()+EYE>0).astype(np.float32))
-        if args.encode_edge_direction:
-            vals.append((mat+EYE>0).astype(np.float32))
-            vals.append((mat.transpose()+EYE>0).astype(np.float32))
-    vals = [sparse.as_coo(x) for x in vals]
-    vals = sparse.stack(vals, axis=0)
-    vals = vals.todense()
+        for mat in edge_feat_list:
+            vals.append((mat+mat.transpose()+EYE>0).astype(np.float32))
+            if args.encode_edge_direction:
+                vals.append((mat+EYE>0).astype(np.float32))
+                vals.append((mat.transpose()+EYE>0).astype(np.float32))
+        vals = [sparse.as_coo(x) for x in vals]
+        vals = sparse.stack(vals, axis=0)
+        vals = vals.todense()
 
-    vals = aml_graph.normalize_adj(vals, args.edge_norm, assume_symmetric_input=False)
-    temp_vals = vals
-    vals = [vals]
-    ret = np.concatenate(vals, 1)
-    edges = np.transpose(ret, [1,2,0])
+        vals = aml_graph.normalize_adj(vals, args.edge_norm, assume_symmetric_input=False)
+        temp_vals = vals
+        vals = [vals]
+        ret = np.concatenate(vals, 1)
+        edges = np.transpose(ret, [1,2,0])
 
-    # ************************************************************
-    # construct model
-    # ************************************************************
-    def layer(layer_type, inputs, dim, training, args, **kwargs):
-        """A wrapper to dispatch different layer construction
-        """
-        if layer_type.lower() == 'gcn':
-            return aml_layers.graph_conv(
-                inputs, dim, training,
-                **kwargs)
-        elif layer_type.lower() == 'gat':
-            return aml_layers.graph_attention(
-                inputs, dim, training,
-                eps=1e-10,
-                # we have a bug in the code for 'dsm' and 'sym' of gat
-                #edge_normalize=args.edge_norm,
-                adaptive=args.adaptive,
-                **kwargs)
-        else:
-            raise ValueError('layer type:', layer_type, ' not supported.')
-
-    # reset computing graph
-    tf.reset_default_graph()
-
-    # input layer
-    training = tf.placeholder(dtype=tf.bool, shape=())
-    h, E0, E1 = nodes, edges[:,:,0], edges[:,:,1]
-
-    # hidden layers
-    h, E0, E1 = layer(args.layer_type, (h, E0, E1, alpha_val), 64, training, args, activation=tf.nn.elu)
-
-    # classification layer
-    logits,_,_ = layer(args.layer_type, (h, E0, E1, alpha_val), nC, training, args,
-                     multi_edge_aggregation='mean')
-    Yhat = tf.one_hot(tf.argmax(logits, axis=-1), nC)
-    loss_train = utils.calc_loss(Y, logits, idx_train, W=W)
-    loss_val = utils.calc_loss(Y, logits, idx_val)
-    loss_test = utils.calc_loss(Y, logits, idx_test)
-
-    vars = tf.trainable_variables()
-    lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in vars if
-                       'bias' not in v.name and
-                       'gamma' not in v.name]) * args.weight_decay
-    optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
-    train_op = optimizer.minimize(loss_train + lossL2)
-
-    init_op = tf.group(tf.global_variables_initializer(),
-                       tf.local_variables_initializer())
-
-    # ************************************************************
-    # training
-    # ************************************************************
-    ckpt_dir = Path('./ckpt')
-    ckpt_dir.mkdir(parents=True, exist_ok=True)
-    ckpt_path = ckpt_dir/'checkpoint.ckpt'
-    print('ckpt_path=', ckpt_path)
-    bad_epochs = 0
-    loss_stop = math.inf
-    acc_stop = -math.inf
-    saver = tf.train.Saver()
-    nan_happend = False
-    with tf.Session() as sess:
-        sess.run(init_op)
-
-        t0 = time.time()
-        for epoch in range(args.epochs):
-            t = time.time()
-            # training step
-            sess.run([train_op], feed_dict={training:True})
-
-            # validation step
-            [loss_train_np, loss_val_np, Yhat_np] = sess.run(
-                [loss_train, loss_val, Yhat],
-                feed_dict={training:False})
-            acc_train = utils.calc_acc(Y, Yhat_np, idx_train)
-            acc_val = utils.calc_acc(Y, Yhat_np, idx_val)
-            acc_test = utils.calc_acc(Y, Yhat_np, idx_test)
-
-            results[idx + tuple([epoch,0])] = loss_val_np
-            results[idx + tuple([epoch,1])] = acc_val
-            results[idx + tuple([epoch,2])] = acc_test
-
-            np.save("egnn_" + dataset + "_final_acc",results)
-
-            if np.isnan(loss_train_np):
-                nan_happend = True
-                print('NaN loss, stop!')
-                break
-
-            print('Epoch=%d, loss=%.4f, acc=%.4f | val: loss=%.4f, acc=%.4f | test acc=%.4f t=%.4f' %
-                  (epoch, loss_train_np, acc_train, loss_val_np, acc_val, acc_test, time.time()-t))
-            if loss_val_np <= loss_stop:
-                bad_epochs = 0
-                # if not args.no_test:
-                #     saver.save(sess, str(ckpt_path))
-                loss_stop = loss_val_np
-                acc_stop = acc_val
+        # ************************************************************
+        # construct model
+        # ************************************************************
+        def layer(layer_type, inputs, dim, training, args, **kwargs):
+            """A wrapper to dispatch different layer construction
+            """
+            if layer_type.lower() == 'gcn':
+                return aml_layers.graph_conv(
+                    inputs, dim, training,
+                    **kwargs)
+            elif layer_type.lower() == 'gat':
+                return aml_layers.graph_attention(
+                    inputs, dim, training,
+                    eps=1e-10,
+                    # we have a bug in the code for 'dsm' and 'sym' of gat
+                    #edge_normalize=args.edge_norm,
+                    adaptive=args.adaptive,
+                    **kwargs)
             else:
-                bad_epochs += 1
-                if bad_epochs == args.patience:
-                    print('Early stop - loss=%.4f acc=%.4f' % (loss_stop, acc_stop))
-                    print('totoal time {}'.format(
-                        datetime.timedelta(seconds=time.time()-t0)))
+                raise ValueError('layer type:', layer_type, ' not supported.')
+
+        # reset computing graph
+        tf.reset_default_graph()
+
+        # input layer
+        training = tf.placeholder(dtype=tf.bool, shape=())
+        h, E0, E1 = nodes, edges[:,:,0], edges[:,:,1]
+
+        # hidden layers
+        h, E0, E1 = layer(args.layer_type, (h, E0, E1, alpha_val), 64, training, args, activation=tf.nn.elu)
+
+        # classification layer
+        logits,_,_ = layer(args.layer_type, (h, E0, E1, alpha_val), nC, training, args,
+                         multi_edge_aggregation='mean')
+        Yhat = tf.one_hot(tf.argmax(logits, axis=-1), nC)
+        loss_train = utils.calc_loss(Y, logits, idx_train, W=W)
+        loss_val = utils.calc_loss(Y, logits, idx_val)
+        loss_test = utils.calc_loss(Y, logits, idx_test)
+
+        vars = tf.trainable_variables()
+        lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in vars if
+                           'bias' not in v.name and
+                           'gamma' not in v.name]) * args.weight_decay
+        optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
+        train_op = optimizer.minimize(loss_train + lossL2)
+
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+
+        # ************************************************************
+        # training
+        # ************************************************************
+        ckpt_dir = Path('./ckpt')
+        ckpt_dir.mkdir(parents=True, exist_ok=True)
+        ckpt_path = ckpt_dir/'checkpoint.ckpt'
+        print('ckpt_path=', ckpt_path)
+        bad_epochs = 0
+        loss_stop = math.inf
+        acc_stop = -math.inf
+        saver = tf.train.Saver()
+        nan_happend = False
+        with tf.Session() as sess:
+            sess.run(init_op)
+
+            t0 = time.time()
+            for epoch in range(args.epochs):
+                t = time.time()
+                # training step
+                sess.run([train_op], feed_dict={training:True})
+
+                # validation step
+                [loss_train_np, loss_val_np, Yhat_np] = sess.run(
+                    [loss_train, loss_val, Yhat],
+                    feed_dict={training:False})
+                acc_train = utils.calc_acc(Y, Yhat_np, idx_train)
+                acc_val = utils.calc_acc(Y, Yhat_np, idx_val)
+                acc_test = utils.calc_acc(Y, Yhat_np, idx_test)
+
+                # results[idx + tuple([epoch,0])] = loss_val_np
+                # results[idx + tuple([epoch,1])] = acc_val
+                # results[idx + tuple([epoch,2])] = acc_test
+                test_accs[alpha_idx,trial,epoch,0], test_accs[alpha_idx,trial,epoch,1] = loss_train_np, loss_val_np
+                test_accs[alpha_idx,trial,epoch,2], test_accs[alpha_idx,trial,epoch,3] =  acc_train, acc_val
+
+                np.save("egnn_" + dataset + "learncurvs",results)
+
+                if np.isnan(loss_train_np):
+                    nan_happend = True
+                    print('NaN loss, stop!')
                     break
 
-        # evaluation step
-        # load check point
-        # if not args.no_test or nan_happend:
-        #     saver.restore(sess, str(ckpt_path))
-        #     [loss_test_np, Yhat_np] = sess.run(
-        #         [loss_test, Yhat], feed_dict={training:False})
-        #     acc = utils.calc_acc(Y, Yhat_np, idx_test)
-        #     print('Testing - loss=%.4f acc=%.4f' % (loss_test_np, acc))
-        # test_accs[idx] = acc
-        # np.save("egnn_test_citeseer_params",test_accs)
+                print('Epoch=%d, loss=%.4f, acc=%.4f | val: loss=%.4f, acc=%.4f | test acc=%.4f t=%.4f' %
+                      (epoch, loss_train_np, acc_train, loss_val_np, acc_val, acc_test, time.time()-t))
+                if loss_val_np <= loss_stop:
+                    bad_epochs = 0
+                    # if not args.no_test:
+                    #     saver.save(sess, str(ckpt_path))
+                    loss_stop = loss_val_np
+                    acc_stop = acc_val
+                else:
+                    bad_epochs += 1
+                    if bad_epochs == args.patience:
+                        print('Early stop - loss=%.4f acc=%.4f' % (loss_stop, acc_stop))
+                        print('totoal time {}'.format(
+                            datetime.timedelta(seconds=time.time()-t0)))
+                        break
+
+            # evaluation step
+            # load check point
+            # if not args.no_test or nan_happend:
+            #     saver.restore(sess, str(ckpt_path))
+            #     [loss_test_np, Yhat_np] = sess.run(
+            #         [loss_test, Yhat], feed_dict={training:False})
+            #     acc = utils.calc_acc(Y, Yhat_np, idx_test)
+            #     print('Testing - loss=%.4f acc=%.4f' % (loss_test_np, acc))
+            # test_accs[idx] = acc
+            # np.save("egnn_test_citeseer_params",test_accs)
