0c5a5a4a6d9bf4343ab4a1561bfcb9b198b86f25
LuisCGit <luis.costa.laveron@gmail.com>
2021-02-18 23:27:11 +0100

    trainvol fix

--- Disso_git/egnn/main_og.py
+++ Disso_git/egnn/main_og.py
@@ -144,6 +144,9 @@ init_op = tf.group(tf.global_variables_initializer(),
 # training
 # ************************************************************
 for tv, train_vol_val in enumerate(train_vol_vals):
+    shuff = idx_train + idx_val + idx_test
+    t,v = int(train_vol_val*len(shuff)), int(len(shuff)*(1-train_vol_val)/2)
+    idx_train, idx_val, idx_test = shuff[:t], shuff[t:t+v], shuff[t+v:]
     for trial in range(args.trials):
 
         ckpt_dir = Path('./ckpt')
