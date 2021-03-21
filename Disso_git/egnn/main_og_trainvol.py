fc33cbd9694643960944123b59039c3b3cdfdf6b
LuisCGit <luis.costa.laveron@gmail.com>
2021-03-21 10:36:12 +0000

    CS for og trianvol

--- Disso_git/egnn/main_og_trainvol.py
+++ Disso_git/egnn/main_og_trainvol.py
@@ -62,7 +62,7 @@ for tv, dic in enumerate(idx_dicts):
     if args.data == 'CS':
         with open('data_coauthor_' + args.data + '/' + args.data + '/curvatures_and_idx/curv_idx','rb') as f:
             X,Y,idx_train,idx_val,idx_test,orc,frc = pickle.load(f)
-            A = nx.linalg.graphmatrix.adjacency_matrix(G)
+            A = nx.linalg.graphmatrix.adjacency_matrix(orc.G)
     else:
         X, Y, A, idx_train, idx_val, idx_test = utils.load_data(args)
     K = A.shape[1] if X is None else X.shape[0]
